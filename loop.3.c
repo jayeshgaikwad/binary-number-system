cad65d0995a554e4ff111ce7c72116f8da4da396
jayeshgaikwad <82097292+jayeshgaikwad@users.noreply.github.com>
2021-04-16 15:56:37 +0530

    Add files via upload

---
+++ loop.3.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main()
+{
+	//1. loop variable
+	//2.condition
+	int i=0;
+	int j=0;
+	for(i=1;i<=6;i++)
+	{
+	
+		for(j=1;j<=i;j++)
+		{
+			printf("0\t");
+			{
+				printf("1\t");
+			}
+			
+		}
+		printf("\n");
+	}
+	
+}
